0a698264f101b477f2f7030b1ef7a96b4c6f5230
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-10-09 13:17:53 +0200

    Hello

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -1,12 +1,17 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hello {
+
+    @Value("${enviromentName}")
+    private String enviromentName;
+
     @GetMapping("/hello")
     public String hello() {
-        return "hello!";
+        return "hello!"+ enviromentName;
     }
 }
